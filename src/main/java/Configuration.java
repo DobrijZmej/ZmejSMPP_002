2354ce0184fced1dabbe50ee5ecb520c1e4a8fc5
DobrijZmej <DobrijZmej@gmail.com>
2019-03-05 21:50:36 +0200

    Борюсь с конфигами, плюс новые исключения в проекте

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,19 +1,46 @@
 import org.yaml.snakeyaml.Yaml;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.*;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Configuration {
+    private final String fileName = "config.yaml";
     private Yaml yaml;
-    private Map<String, Object> data;
+    private Map<String, Object> data = new LinkedHashMap<>();
 
     public Configuration(){
         yaml = new Yaml();
-        InputStream file = this.getClass().getClassLoader().getResourceAsStream("config.yaml");
+        InputStream is = this.getClass().getClassLoader().getResourceAsStream(fileName);
 //        System.out.println(this.getClass().getClassLoader().getResourceAsStream("log4j.properties"));
-        if(file != null) {
-            data = yaml.load(file);
+        if(is != null) {
+            data = yaml.load(is);
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Map<String, Object> general = new LinkedHashMap<>();
+            general.put("Port", 2775);
+            data.put("General", general);
+            StringWriter sw = new StringWriter();
+            yaml.dump(data, sw);
+//            FileWriter fw = null;
+            try {
+//                System.out.println(this.getClass().getResource("").getPath());
+//                File f = new File(this.getClass().getResource("").getPath())
+                FileWriter fw = new FileWriter(this.getClass().getResource("").getPath()+fileName);
+                fw.write(sw.toString());
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                sw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         };
         System.out.println(data);
     }
