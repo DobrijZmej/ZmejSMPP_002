b882da1e2f22e89ddc6750551300b61720ceba09
DobrijZmej <DobrijZmej@gmail.com>
2019-03-05 22:19:42 +0200

    Разбил конфиг на методы

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -5,14 +5,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Configuration {
-    private final String fileName = "config.yaml";
+    private final String fileName = "config.yml";
     private Yaml yaml;
     private Map<String, Object> data = new LinkedHashMap<>();
 
     public Configuration(){
         yaml = new Yaml();
         InputStream is = this.getClass().getClassLoader().getResourceAsStream(fileName);
-//        System.out.println(this.getClass().getClassLoader().getResourceAsStream("log4j.properties"));
         if(is != null) {
             data = yaml.load(is);
             try {
@@ -21,27 +20,27 @@ public class Configuration {
                 e.printStackTrace();
             }
         } else {
-            Map<String, Object> general = new LinkedHashMap<>();
-            general.put("Port", 2775);
-            data.put("General", general);
-            StringWriter sw = new StringWriter();
-            yaml.dump(data, sw);
-//            FileWriter fw = null;
-            try {
-//                System.out.println(this.getClass().getResource("").getPath());
-//                File f = new File(this.getClass().getResource("").getPath())
-                FileWriter fw = new FileWriter(this.getClass().getResource("").getPath()+fileName);
-                fw.write(sw.toString());
-                fw.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                sw.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            setDefault();
+            dump();
         };
-        System.out.println(data);
+//        System.out.println(data);
+    }
+
+    private void setDefault(){
+        Map<String, Object> general = new LinkedHashMap<>();
+        general.put("Port", 2775);
+        data.put("General", general);
+    }
+
+    private void dump() {
+        String filePath = this.getClass().getResource("").getPath()+fileName;
+        try (StringWriter sw = new StringWriter();
+               FileWriter fw = new FileWriter(filePath);) {
+            yaml.dump(data, sw);
+            fw.write(sw.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
