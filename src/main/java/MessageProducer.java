3161b9b228d1be22fbee4e68534dde4c5bf27271
DobrijZmej <DobrijZmej@gmail.com>
2019-03-11 22:42:50 +0200

    Получилось собрать данные из сокета, и отправить на обработку

--- src/main/java/MessageProducer.java
+++ src/main/java/MessageProducer.java
@@ -1,6 +1,7 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.naming.ldap.SortKey;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,23 +23,24 @@ public class MessageProducer implements Runnable {
         Configuration conf = new Configuration();
         int port = conf.readPort();
         server = new ServerSocket(port);
-        logger.info("Server listener start on port "+port);
+        logger.info("Server listener start on port " + port);
         this.queue = queue;
     }
 
     @Override
     public void run() {
 
-        while(true){
+        while (true) {
             try {
                 fromclient = this.server.accept();
-                StartSession(fromclient);
+                startSession(fromclient);
+//                startSession(fromclient);
                 fromclient.close();
-                Thread.sleep(1000);
+//                Thread.sleep(1000);
                 logger.info("Waiting data");
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                return;
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//                return;
             } catch (IOException e) {
                 e.printStackTrace();
                 return;
@@ -47,20 +49,86 @@ public class MessageProducer implements Runnable {
 
     }
 
-    private void StartSession(Socket fromclient) {
+    /**
+     * Очікування даних від клієнта з черги
+     *
+     * @param fromClient
+     */
+    private void startSession(Socket fromClient){
+        byte[] buffer = readData(fromClient);
+//        System.out.println(buffer);
+        if(buffer.length>0){
+            decodeData(buffer);
+        }
+
+
+
+    }
+
+    /**
+     * Дешифрування надісланих даних
+     *
+     * @param buffer
+     */
+    private void decodeData(byte[] buffer) {
+        for(int i = 0; i<buffer.length; i++){
+            System.out.println(buffer[i]);
+        }
+    }
+
+    /**
+     * Метод чекає надходження даних на порт, та повертає отримані дані
+     *
+     * @param fromClient - сокет, з якого очікуються дані
+     * @return масив отриманих даних
+     */
+    private byte[] readData(Socket fromClient) {
+        byte[] buffer = new byte[1024];
+        byte[] bufferReturn = null;
+        try {
+            int i = fromClient.getInputStream().read(buffer);
+            if(i > -1) {
+                bufferReturn = new byte[i];
+                System.arraycopy(buffer, 0, bufferReturn, 0, i);
+                logger.debug("Read ["+i+"] bytes:");
+                StringBuilder s = new StringBuilder();
+                StringBuilder d = new StringBuilder();
+                for (int r = 0; r<i; r++){
+                    int b = buffer[r];
+                    s.append(b<10?"0"+Integer.toString(b, 16):Integer.toString(b, 16));
+                    d.append(b).append(" ");
+                }
+                logger.debug(s.toString());
+                logger.debug(d.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error create BufferedReader");
+            logger.error(e.getMessage(), e);
+        }
+        return bufferReturn;
+    }
+
+    private void StartSession_old(Socket fromclient) {
 //        BufferedReader in;
         PrintWriter out;
         try {
             DataInputStream in = new DataInputStream(new BufferedInputStream(fromclient.getInputStream()));
 //            in = new BufferedReader(new InputStreamReader(fromclient.getInputStream()));
             out = new PrintWriter(fromclient.getOutputStream(), true);
-            byte[] buffer = new byte[1];
+            byte[] buffer = new byte[1024];
             String s = "";
-            while(in.read(buffer) > 0) {
-//                logger.info("Read " + buffer.length);
-                  String r = String.valueOf((int)buffer[0]);
-                  logger.info(r);
-                  s = s + r;
+//            int i = in.read(buffer);
+            int i = fromclient.getInputStream().read(buffer);
+//            if(i > -1) {
+//                byte[]
+//            }
+            while (i > -1) {
+                logger.info("Read " + i + " bytes");
+                String r = String.valueOf((int) buffer[0]);
+                logger.info(r);
+                s = s + r;
+                i = fromclient.getInputStream().read(buffer);
             }
             logger.info(s);
 //            ReadSesion(in, out);
@@ -75,6 +143,7 @@ public class MessageProducer implements Runnable {
             in.close();
         } catch (IOException e) {
             System.out.println("Error create BufferedReader");
+            logger.error(e.getMessage(), e);
             System.exit(-1);
         }
     }
